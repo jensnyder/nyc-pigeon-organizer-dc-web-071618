90d4e51429494d0de464383bf567230ef6f06b44
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-25 14:42:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |characteristic, characteristic_hash|
+    characteristic_hash.each do |trait, trait_array|
+      trait_array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][characteristic] == nil
+          pigeon_list[name][characteristic] = []
+        end
+        if data[characteristic][trait].include?(name)
+          pigeon_list[name][characteristic] << trait.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
